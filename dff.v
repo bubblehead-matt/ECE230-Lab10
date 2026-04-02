c548a1abc95b27ece15ca47bbfc01b4637acb924
Matt DeRoos <MattDeRoos@u.boisestate.edu>
2026-04-01 20:27:49 -0600

    Implement D flip-flop module in Verilog

--- dff.v
+++ dff.v
@@ -1,1 +1,18 @@
+module dff (
+  input D,
+  input Clock,
+  output reg Q,
+  output NotQ
+);
+  
+initial begin
+    Q <= 0;
+end
 
+always @(posedge Clock) begin
+    Q <= D;
+end
+
+assign NotQ = ~Q;
+  
+endmodule
