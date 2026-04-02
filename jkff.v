bb923f8b97591873ac828d163ef852925c539851
Matt DeRoos <MattDeRoos@u.boisestate.edu>
2026-04-01 20:56:55 -0600

    Implement JK flip-flop module in Verilog

--- jkff.v
+++ jkff.v
@@ -1,1 +1,22 @@
+module jkff (
+  input J, K, Clock,
+  output reg Q,
+  output NotQ
+);
+  
+  initial begin
+    Q <= 0;
+  end
+  
+  always @(posedge Clock) begin
+      case({J, K})
+          2'b00: Q <= Q;
+          2'b01: Q <= 0;
+          2'b10: Q <= 1;
+          2'b11: Q <= ~Q;
+      endcase
+  end
 
+  assign NotQ = ~Q;
+  
+endmodule
