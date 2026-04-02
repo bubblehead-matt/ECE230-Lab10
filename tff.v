0503b8112b06e2199138a44de52fc9919c3c8fa7
Matt DeRoos <MattDeRoos@u.boisestate.edu>
2026-04-01 21:01:07 -0600

    Implement T Flip-Flop module in Verilog

--- tff.v
+++ tff.v
@@ -1,1 +1,20 @@
-
+module tff (
+    input T, Clock,
+    output reg Q,
+    output notQ
+    );
+   
+   initial begin
+          Q <= 0;
+   end
+    
+   always @(posedge Clock) begin
+        if (T)
+          Q <= ~Q;
+        else
+          Q <= Q;
+   end
+   
+   assign NotQ = ~Q;
+   
+endmodule
